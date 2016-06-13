e62acaec2c154165a9b03476c8c600e9606f7d4c
cbwuyanzu <277850402@qq.com>
2016-06-13 15:29:26 +0800

    barchart

--- public/scripts/modules/sample-module/ctp.js
+++ public/scripts/modules/sample-module/ctp.js
@@ -3,46 +3,37 @@ define(['angular', './sample-module'], function(angular, controllers) {
     controllers.controller('CTPCtrl', ['$scope', function($scope) {
         var points = [{
             id: 0,
-            name: '上海普陀',
-            url: '1141A'
+            name: 'CTP indoor',
+            url: '/api/inquiry-weekly-service/61726'
         }, {
             id: 1,
-            name: '上海十五厂',
-            url: '1142A'
+            name: 'CTP outdoor',
+            url: '/api/inquiry-weekly-service/61728'
         }, {
             id: 2,
-            name: '上海虹口',
-            url: '1143A'
+            name: 'CTP indoor',
+            url: '/api/inquiry-service/61726'
         }, {
             id: 3,
-            name: '上海徐汇上师大',
-            url: '1144A'
-        }, {
-            id: 4,
-            name: '上海杨浦四漂',
-            url: '1145A'
-        }, {
-            id: 5,
-            name: '上海静安监测站',
-            url: '1147A'
-        }, {
-            id: 6,
-            name: '上海浦东川沙',
-            url: '1148A'
-        }, {
-            id: 7,
-            name: '上海浦东新区监测站',
-            url: '1149A'
-        }, {
-            id: 8,
-            name: '上海浦东张江',
-            url: '1150A'
+            name: 'CTP outdoor',
+            url: '/api/inquiry-service/61728'
         }];
         var newcontext = {
-            name: 'pm 2.5 in Shanghai'
+            name: 'Daily pm 2.5',
+            name1: 'Hourly pm 2.5',
+            starts: []
         };
+        var start = [{
+            id: 0,
+            name: '1d',
+            starttime: '?starttime=1d-ago'
+        }, {
+            id: 1,
+            name: '7d',
+            starttime: '?starttime=7d-ago'
+        }];
         $scope.context = newcontext;
         $scope.context.urls = points;
-        $scope.context.url = '1141A';
+        $scope.context.starts = start;
     }]);
 });
